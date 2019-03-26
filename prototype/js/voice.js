8ee6f340b9985e2ed9694465f96b5a51a517d9aa
Julia Pritzen <julia.pritzen@gmail.com>
2019-03-27 00:48:34 +0100

    cancelled ongoing speech utterance on new page, minor refactoring

--- prototype/js/voice.js
+++ prototype/js/voice.js
@@ -1,20 +1,25 @@
 // speech synthesis with Web Spech API
 
-// play tts output
 $(window).on('load', function () {
-    // get all elements that include voice attribute & generate tts output
-    $('*[voice]').each(function () {
-        var voiceOutput = new SpeechSynthesisUtterance();
-        voiceOutput.text = $(this).text();
-        voiceOutput.lang = 'de-DE';
-        voiceOutput.rate = 1.0;
-        speechSynthesis.speak(voiceOutput);
-    });
-
-    // show voice warning when browser == chrome (only on index.html)
-    if (window.location.href.includes('index.html')) {
+    // show voice warning when browser == chrome (checking for class that only exists on index.html)
+    if ($('.message-container')) {
         if (/Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)) {
             $('.voice-warning').css('visibility', 'visible');
         }
     }
+
+    // cancel ongoing speech utterance on new page
+    speechSynthesis.cancel();
+
+    //play tts output after a small timeout (workaround after cancel)
+    setTimeout(function () {
+        // get all elements that include voice attribute & generate tts output
+        $('*[voice]').each(function () {
+            var voiceOutput = new SpeechSynthesisUtterance();
+            voiceOutput.text = $(this).text();
+            voiceOutput.lang = 'de-DE';
+            voiceOutput.rate = 1.0;
+            speechSynthesis.speak(voiceOutput);
+        });
+    }, 100);
 });
