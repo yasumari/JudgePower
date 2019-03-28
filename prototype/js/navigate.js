833a1f8ab0e75daf487cbe2d56fa5591475b6369
Julia Pritzen <julia.pritzen@gmail.com>
2019-03-28 10:20:10 +0100

    added status bar to show progress

--- prototype/js/navigate.js
+++ prototype/js/navigate.js
@@ -1,4 +1,19 @@
-// navigate on keypress
+// navigate on keypress & set status bar
+
+// set width for status bar (shitty and manually...)
+$(window).on('load', function () {
+    // fill array with respective pages and calculate bar length
+    var questions = ["question-1.html", "question-2.html", "question-3.html", "question-4.html", "end.html"];
+    var previousBarLength = (100 / questions.length) * (questions.indexOf(window.location.href.split("/").pop()));
+    var barLength = (100 / questions.length) * (questions.indexOf(window.location.href.split("/").pop()) + 1);
+    // set previous length of status bar for better transitioning
+    $('.status-bar').css('width', previousBarLength + '%');
+    //workaround: small timeout because values were set simultaneously
+    setTimeout(function () {
+        $('.status-bar').css('transition', 'width 0.5s cubic-bezier(0, 1, 0.5, 1)');
+        $('.status-bar').css('width', barLength + '%');
+    }, 100);
+});
 
 // check if element includes key input
 $(document).keypress(function (event) {
@@ -20,7 +35,7 @@ $(document).keypress(function (event) {
     // get navigate attribute
     var navigate = $(matchingAnimationElements).attr('navigate')
 
-    // check if navigate attrinbute of element is present
+    // check if navigate attribute of element is present
     if (typeof navigate !== typeof undefined && navigate !== false) {
         // wait for end of css animation, then navigate to next page
         $(matchingAnimationElements).one('webkitAnimationEnd oanimationend msAnimationEnd animationend',
